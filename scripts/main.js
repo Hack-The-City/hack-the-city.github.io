339f9b5d50156ba55c91f2892f9bfb577ddd4208
Chelesa Valentine <cv851@nyu.edu>
2015-10-03 04:21:25 -0400

    add navbar implementation

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,83 @@
 (function(){
-  // alert("hi");
+  initializeNavigation();
+  getSectionYPositions();
 })();
+
+function initializeNavigation() {
+  var options = document.querySelectorAll("header a");
+
+  for (var i = 0; i < options.length; i++) {
+    options[i].addEventListener("click", function() {
+      selectMenuOption(this);
+    });
+  }
+}
+
+function selectMenuOption(option) {
+  var options = document.querySelectorAll("header a");
+
+  for (var j = 0; j < options.length; j++) {
+    options[j].classList.remove("active");
+  }
+  option.classList.add("active");
+}
+var aboutYPos, faqYPos, sponsorsYPos;
+
+function getSectionYPositions() {
+  var splash = 0,
+      about = document.getElementById("about"),
+      faq = document.getElementById("faq"),
+      sponsors = document.getElementById("sponsors");
+
+  aboutYPos = $(about).offset().top;
+  faqYPos = $(faq).offset().top;
+  sponsorsYPos = $(sponsors).offset().top;
+}
+
+function updateMenuOptionSelection() {
+  var options = document.querySelectorAll("header a"),
+      aboutOption = options[0],
+      faqOption = options[1],
+      sponsorsOption = options[2],
+      registerOption = options[3],
+      error = 250;
+
+  if ($(window).scrollTop() < aboutYPos + error) {
+    selectMenuOption(registerOption);
+  } else if ($(window).scrollTop() < faqYPos + error) {
+    selectMenuOption(aboutOption);
+  } else if ($(window).scrollTop() < sponsorsYPos + error) {
+    selectMenuOption(faqOption);
+  } else {
+    selectMenuOption(sponsorsOption);
+  }
+}
+
+$(window).resize(adjustBanner());
+
+function adjustBanner() {
+  var banner = document.getElementById("entry-point");
+  banner.style.height = window.innerHeight + "px";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*//
+// Initial script
+*///
 
 //This creates a better transition then in document ready.
 $(window).load(function() {
-     $('video').animate({opacity: '1'}, 800);
+     $('video').animate({opacity: '1'}, 400);
 });
 
 var scrollTimeout;
@@ -16,6 +89,7 @@ $(window).on('scroll', function() {
     scrollTimeout = setTimeout(checkAndMoveNavbarPosition(), 250);
     recalculateNavbarPosition();
   }
+  updateMenuOptionSelection();
 });
 
 
@@ -42,10 +116,4 @@ function recalculateNavbarPosition() {
   // console.log($('#navbar').height());
 }
 
-$(window).resize(adjustBanner());
 
-function adjustBanner() {
-  var banner = document.getElementById("entry-point");
-  banner.style.height = window.innerHeight + "px";
-
-}
