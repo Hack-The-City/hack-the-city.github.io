7211f64e2dcfc84f160313d0e6e2dad157b3d3e4
Chelesa Valentine <cv851@nyu.edu>
2015-10-03 05:17:28 -0400

    implement faq filtering

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,8 @@
   getSectionYPositions();
 
   initializeInputField();
+
+  initializeFaq();
 })();
 
 function initializeNavigation() {
@@ -23,6 +25,7 @@ function selectMenuOption(option) {
   }
   option.classList.add("active");
 }
+
 var aboutYPos, faqYPos, sponsorsYPos;
 
 function getSectionYPositions() {
@@ -65,8 +68,14 @@ function initializeInputField() {
         hasAtSymbol = atSymbolPosition > 0,
         hasTextAfterAtSymbol = atSymbolPosition !== value.length - 1;
         hasDotAfterAtSymbol = value.indexOf(".") > atSymbolPosition;
+        hasLettersBeforeDot = (value.indexOf(".") - 1) !== atSymbolPosition;
+        dotIsNotLast = value.indexOf(".") !== value.length - 1;
 
-    if (hasAtSymbol && hasTextAfterAtSymbol && hasDotAfterAtSymbol) {
+    if (hasAtSymbol && 
+        hasTextAfterAtSymbol &&
+        hasDotAfterAtSymbol &&
+        hasLettersBeforeDot &&
+        dotIsNotLast) {
       submitButton.disabled = false;
       submitButton.classList.remove("invalid");
       submitButton.classList.add("valid");
@@ -75,7 +84,81 @@ function initializeInputField() {
       submitButton.classList.remove("valid");
       submitButton.classList.add("invalid");
     }
-  })
+  });
+}
+
+function initializeFaq() {
+  var about = document.getElementById("faq-about"),
+      travel = document.getElementById("faq-travel"),
+      hacking = document.getElementById("faq-hacking"),
+      other = document.getElementById("faq-other"),
+      options = [about, travel, hacking, other];
+
+  about.addEventListener('click', function() {filterFaqBy("about");});
+  travel.addEventListener('click', function() {filterFaqBy("travel");});
+  hacking.addEventListener('click', function() {filterFaqBy("hacking");});
+  other.addEventListener('click', function() {filterFaqBy("other");});
+
+  for (var i = 0; i < options.length; i++) {
+    options[i].addEventListener('click', function() {
+      for (var j = 0; j < options.length; j++) {
+        options[j].classList.remove("selected");
+      }
+      this.classList.add("selected");
+    });
+  }
+}
+
+function filterFaqBy(filter) {
+  switch(filter){
+    case "about":
+      showFaqsRelatingTo("about");
+      break;
+    case "travel":
+      showFaqsRelatingTo("travel");
+      break;
+    case "hacking":
+      showFaqsRelatingTo("hacking");
+      break;
+    case "other":
+      showFaqsRelatingTo("other");
+      break;
+    default:
+      break;
+  }
+}
+
+function showFaqsRelatingTo(className) {
+  var faq_all = document.getElementsByClassName("faq-all"),
+      class_faqs = document.getElementsByClassName("faq-" + className);
+
+  hideUnrelatedFaqs();
+
+  for (var i = 0; i < faq_all.length; i++) {
+    faq_all[i].style.display = 'inline-block';
+  }
+
+  for (var j = 0; j < class_faqs.length; j++) {
+    class_faqs[j].style.display = 'inline-block';
+  }
+}
+
+function hideUnrelatedFaqs() {
+  var faqs = document.getElementsByClassName("faq");
+
+  for (var i = 0; i < faqs.length; i++) {
+    faqs[i].style.display = 'none';
+  }
+}
+
+
+function selectMenuOption(option) {
+  var options = document.querySelectorAll("header a");
+
+  for (var j = 0; j < options.length; j++) {
+    options[j].classList.remove("active");
+  }
+  option.classList.add("active");
 }
 
 $(window).resize(adjustBanner());
